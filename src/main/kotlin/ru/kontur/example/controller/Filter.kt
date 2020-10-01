ad027a1e177ffd8c4116e826b1c0542dcd8743d2
kostya05983 <kostya05983@mail.ru>
2020-10-01 21:49:52 +0700

    Without take

--- src/main/kotlin/ru/kontur/example/controller/Filter.kt
+++ src/main/kotlin/ru/kontur/example/controller/Filter.kt
@@ -3,9 +3,6 @@ package ru.kontur.example.controller
 import org.springframework.web.bind.annotation.RequestParam
 
 data class Filter(
-    @RequestParam
-    val take: Int = 30,
-
     @RequestParam(name = "types[]")
     var types: MutableList<String>?
 )
